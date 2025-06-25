9e8e5e8fd30b1787625762ecb6fe0ba0b59e212e
PadmanabhamDev <padmanabham0502@outlook.com>
2025-06-25 23:32:53 +0530

    record view Form

---
+++ force-app/main/default/lwc/recordViewForm/recordViewForm.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import Account_Object from '@salesforce/schema/Account';
+export default class RecordViewForm extends LightningElement {
+
+    objectName = Account_Object
+}
