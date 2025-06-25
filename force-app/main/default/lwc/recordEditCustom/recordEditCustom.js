3b4663e25338c4901876150d8a82766b82d7d135
PadmanabhamDev <padmanabham0502@outlook.com>
2025-06-25 23:33:38 +0530

    record edit Custom

---
+++ force-app/main/default/lwc/recordEditCustom/recordEditCustom.js
@@ -0,0 +1,38 @@
+import { LightningElement } from 'lwc';
+import Contact_Object from '@salesforce/schema/Account';
+export default class RecordEditCustom extends LightningElement {
+    objectName = Contact_Object;
+    nameValue = "";
+    phoneValue = '';
+    employeesValue = '';
+    handleChange(event) {
+        const { label, value } = event.target;
+        switch (label) {
+            case 'Name':
+                this.nameValue = value;
+                break;
+            case 'Phone':
+                this.phoneValue = value;
+                break;
+            case 'Employees':
+                this.employeesValue = value;
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();//so avaoid auto refresh on submit
+        const inputValidity = this.template.querySelectorAll('lightning-input');
+        let isValid = true;
+        inputValidity.forEach(field => {
+            if (!field.checkValidity()) {
+                field.setCustomValidity("Cannot be Empty Value");
+                isValid = false;
+            } else {
+                field.setCustomValidity("");
+                isValid = true;
+            }
+        })
+
+        console.log(isValid);
+    }
+}
